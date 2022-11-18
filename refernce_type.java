1f54fb88982276e29af5689e1feb5402510533e2
EgERohit2 <118536484+EgERohit2@users.noreply.github.com>
2022-11-18 04:32:11 -0800

    Add files via upload

---
+++ refernce_type.java
@@ -0,0 +1,27 @@
+package types;
+
+import java.util.Date;
+
+public class refernce_type {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=20;        //primitive datatype
+		float f=(float) 20.9;
+		double d=88.99;
+		boolean b=true;
+		char c='R';
+		
+		System.out.println(age);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		
+		
+		Date now=new Date();
+		System.out.println(now);
+
+	}
+
+}
